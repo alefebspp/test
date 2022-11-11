109acb694571cad4ba9137f0e20c852aa9bacd20
Alefe Bispo <alefebsp@hotmail.com>
2022-11-11 18:53:38 -0300

    defined getEmployee, deleteEmployee, getAllEmployees and updateEmployee functions on EmployeeController

--- back-end/src/controller/EmployeeController.js
+++ back-end/src/controller/EmployeeController.js
@@ -1,6 +1,26 @@
 const Employee = require('../model/Employee');
 
 module.exports = {
+  async getAllEmployees(req, res) {
+    try {
+      const employees = await Employee.find();
+      res.status(200).json(employees);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+  async getEmployee(req, res) {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ _id: id });
+    try {
+      if (!employee) {
+        res.status(400).json(console.log('Could not find employee!'));
+      }
+      res.status(200).json(employee);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
   async createEmployee(req, res) {
     const { firstName, lastName, age, profession, sector } = req.body;
     const employee = new Employee({
@@ -15,6 +35,40 @@ module.exports = {
       res.status(200).json(employee);
     } catch (error) {
       res.status(400).json(error);
+    }
+  },
+  async deleteEmployee(req, res) {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ _id: id });
+    try {
+      if (!employee) {
+        res.status(400).json(console.log('Could not find employee!'));
+      }
+      const deletedEmployee = await Employee.deleteOne(employee);
+      res.status(200).json(deletedEmployee);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+  async updateEmployee(req, res) {
+    const { id } = req.params;
+    const { firstName, lastName, age, profession, sector } = req.body;
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      profession: profession,
+      sector: sector
+    };
+    const employee = await Employee.findOne({ _id: id });
+    try {
+      if (!employee) res.status(401).json('Could not find employee!');
+      else {
+        const updatedEmployee = await Employee.updateOne(employee, data);
+        res.status(201).json(updatedEmployee);
+      }
+    } catch (error) {
+      res.status(401).json(error);
     }
   }
 };
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -4,5 +4,8 @@ const EmployeeController = require('../controller/EmployeeController');
 const routes = Router();
 
 routes.post('/employee/create', EmployeeController.createEmployee);
-
+routes.get('/employee/all', EmployeeController.getAllEmployees);
+routes.get('/employee/:_id', EmployeeController.getEmployee);
+routes.delete('/employee/delete/:_id', EmployeeController.deleteEmployee);
+routes.patch('/employee/update/:_id', EmployeeController.updateEmployee);
 module.exports = routes;
